d6f51314783fadb1f4930f116f445ed189706166
git config --global user.name "hyk68691"git config --global user.name "Your Name Here"git config --global user.name "Your Name Here"git config --global user.name "Your Name Here <git config --global user.email "hyk68691@gmail.com>
2016-03-08 09:30:06 +0800

    Login page  added.

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -4,6 +4,34 @@ class SessionController < ApplicationController
   end
 
   def create
+    puts params
+    status = params[:session][:status]
 
+    if status
+      if status == 'Examinee'
+        user = Examinee.find_by_username params[:session][:username]
+        if user && user.password == params[:session][:password]
+          flash[:success] = "Login success as examinee"
+          session[:user_id] = user.id
+          session[:status] = 'Examinee'
+        else flash[:danger] = "Invalid username or password."
+        end
+
+      elsif status == 'Examiner'
+        user = Examiner.find_by_username params[:session][:username]
+        if user && user.password == params[:session][:password]
+          flash[:success] = "Login success as examiner"
+          session[:user_id] = user.id
+          session[:status] = 'Examiner'
+        else flash[:danger] = "Invalid username or password."
+        end
+
+      else flash[:danger] = "Invalid status"
+      end
+    else
+      flash[:danger] = "Empty status"
+    end
+
+    redirect_to login_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'login' => 'session#new'
+  post 'login' => 'session#create'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
