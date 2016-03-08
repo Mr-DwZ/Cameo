990f2c2afc142308657b5e0a0deed0449eefca44
git config --global user.name "hyk68691"git config --global user.name "Your Name Here"git config --global user.name "Your Name Here"git config --global user.name "Your Name Here <git config --global user.email "hyk68691@gmail.com>
2016-03-08 10:32:21 +0800

    Examiner's controller and show view added.

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -9,27 +9,25 @@ class SessionController < ApplicationController
 
     if status
       if status == 'Examinee'
-        user = Examinee.find_by_username params[:session][:username]
         if user && user.password == params[:session][:password]
           flash[:success] = "Login success as examinee"
           session[:user_id] = user.id
           session[:status] = 'Examinee'
-        else flash[:danger] = "Invalid username or password."
+        else
+          flash[:danger] = "Invalid username or password."
         end
-
       elsif status == 'Examiner'
         user = Examiner.find_by_username params[:session][:username]
         if user && user.password == params[:session][:password]
           flash[:success] = "Login success as examiner"
           session[:user_id] = user.id
           session[:status] = 'Examiner'
-        else flash[:danger] = "Invalid username or password."
+        else
+          flash[:danger] = "Invalid username or password."
         end
-
       else flash[:danger] = "Invalid status"
       end
-    else
-      flash[:danger] = "Empty status"
+    else flash[:danger] = "Empty status"
     end
 
     redirect_to login_path
