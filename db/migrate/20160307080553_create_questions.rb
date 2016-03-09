57ac0a395a358c11f4e33c51df80eed29ae15d3e
git config --global user.name "hyk68691"git config --global user.name "Your Name Here"git config --global user.name "Your Name Here"git config --global user.name "Your Name Here <git config --global user.email "hyk68691@gmail.com>
2016-03-09 20:06:18 +0800

    Question migration changed.

--- db/migrate/20160307080553_create_questions.rb
+++ db/migrate/20160307080553_create_questions.rb
@@ -1,7 +1,15 @@
 class CreateQuestions < ActiveRecord::Migration
   def change
     create_table :questions do |t|
-      t.string  :properties
+      t.text :description
+      t.text :input
+      t.text :output
+      t.text :sample_input
+      t.text :sample_output
+      t.text :hint
+      t.text :test_input
+      t.text :test_output
+
       t.integer :exam_id
       t.integer :examiner_id
 
