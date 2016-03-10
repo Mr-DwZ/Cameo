41b579fa4934d0a6c8e2180b197d980dbf2df200
git config --global user.name "hyk68691"git config --global user.name "Your Name Here"git config --global user.name "Your Name Here"git config --global user.name "Your Name Here <git config --global user.email "hyk68691@gmail.com>
2016-03-10 22:07:40 +0800

    Remove a examinee from a exam operation finished.

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -4,16 +4,15 @@ class ResultsController < ApplicationController
   end
 
   def create
-    @result = Result.new
-    @result.examinee = Examinee.find_by_id params[:result][:examinee_id]
-    @result.exam = Exam.find_by_id params[:exam_id]
-    @result.save
-    @exam = Exam.find_by_id session[:user_id]
-    @exam.results << @result
+    @exam = Exam.find_by_id params[:exam_id]
+    @examinee = Examinee.find_by_id params[:result][:examinee_id]
+    @exam.examinees << @examinee
     redirect_to examiner_exam_path(params[:examiner_username], params[:exam_id])
   end
 
   def destroy
-
+    result = Result.find_by_id params[:id]
+    result.destroy
+    redirect_to examiner_exam_path(params[:examiner_username], params[:exam_id])
   end
 end
