ff20f372b09da9660bb662a2e0ea11234f720be0
git config --global user.name "hyk68691"git config --global user.name "Your Name Here"git config --global user.name "Your Name Here"git config --global user.name "Your Name Here <git config --global user.email "hyk68691@gmail.com>
2016-03-10 15:50:38 +0800

    Examinee's routes added.

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,14 @@ Rails.application.routes.draw do
       resources :questions
     end
   end
+
+  resources :examinees, param: :username do
+    resources :exams, only: [:index, :show]do
+      resources :questions, only:[:index, :show] do
+        resource :code
+      end
+    end
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
